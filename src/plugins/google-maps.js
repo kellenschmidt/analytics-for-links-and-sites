887b758c5b5c01c51eeb2b9dc4e49e4b2d5956d0
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-08-31 01:32:04 -0500

    Hardcode key, because Vue substitutes env vars at build-time not runtime

--- src/plugins/google-maps.js
+++ src/plugins/google-maps.js
@@ -3,7 +3,8 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY,
+    // key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY,
+    key: 'AIzaSyA_CyjjZ1VMw6pzAwFlBEXTufO7B64rD8w'
     // libraries: 'places',
   },
 })
