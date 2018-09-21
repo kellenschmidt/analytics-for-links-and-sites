3684ca8d5a7c11011ec2705aab72602c1e9d911a
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-09-20 23:00:36 -0500

    Fix colors but don't upgrade packages

--- src/components/echarts/EchartPie.vue
+++ src/components/echarts/EchartPie.vue
@@ -79,7 +79,8 @@ export default {
               }
             },
             label: {
-              position: this.size == 'big' ? 'outside' : 'inside'
+              position: this.size == 'big' ? 'outside' : 'inside',
+              color: this.size == 'big' ? '' : '#000',
             }
           }
         ]
